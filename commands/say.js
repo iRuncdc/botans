ed5d5ce7c59345bcc0733daff086d2d7cb3eb408
Glitch (hello-express) <none>
2020-12-26 18:36:32 +0000

    🚄🚜 Checkpoint ./commands/slowmode.js:26554501/2951 ./commands/mute.js:26554501/3699 ./commands/say.js:26554501/235 ./commands/afk.js:26554501/6236 ./commands/unmute.js:26554501/2284 ./watch.json:26554501/2

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,7 @@
 exports.run = async(client, message, args) => {
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.')
+  
   let say = args.join(' ')
   message.channel.send(say)
 }
