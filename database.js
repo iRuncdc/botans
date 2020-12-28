f7dfcd5f497c70f742ba12da62a575f750ea11a2
Glitch (hello-express) <none>
2020-12-28 03:04:49 +0000

    👱🍃 Checkpoint ./watch.json:26554501/8 ./database.js:26554501/1648 ./server.js:26554501/3949 ./commands/start.js:26554501/2836

--- database.js
+++
@@ -1,8 +0,0 @@
-const { Database } = require('quickmongo');
-const db = new Database("mongodb://localhost/testingggg");
-
-db.on('ready', () => {
-    console.log('Connected to mongoose')
-})
-
-module.exports = db;
