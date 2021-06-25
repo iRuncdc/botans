7a5a7a67e1b776f1045fd5444eb9ce785cc40f86
Glitch (isekaiiiggggeming) <none>
2021-06-25 06:26:19 +0000

    👨‍❤️‍👨🐂 Checkpoint ./server.js:26895190/1118 ./watch.json:26895190/4

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ client.on('message', message => {
 
 
 
-module.exports = async (client, oldMember, newMember, message) => {
+client.on = async (client, oldMember, newMember, message) => {
   //booost
   if (!oldMember.premiumSince && newMember.premiumSince ) {
     let embed = new Discord.MessageEmbed()
@@ -147,5 +147,31 @@ Dont Forget To Contact Server Manager To Claim The Benefit
     return client.channels.cache.get("685332210785845287").send(embed)
   }
 }
+
+//boost
+
+client.on('nitroBoost', (booster,member) => {
+const channel = member.guild.channels.cache.find(ch => ch.id === '685332210785845287');
+if (!channel) return;
+ let embed = new Discord.MessageEmbed()
+//embed
+ .setAuthor('Thank You **${newMember.user.tag}**', 'https://cdn.discordapp.com/emojis/707059456952762471.gif?v=1')
+  .setThumbnail('')
+  .setDescription(`
+Dont Forget To Contact Server Manager To Claim The Benefit
+
+<@&683712552785412127>`)
+  .setColor('#ef5dff')
+  .setImage('https://cdn.discordapp.com/attachments/832074883898146846/853954792937619486/ezgif-7-2dec1aa99780.gif')
+  .setTimestamp()
+  .setFooter(
+'ISEI BOOSTER', 
+"https://cdn.discordapp.com/icons/675998580019953675/a_b90104dbbbe9d5623d5814cba33d2ac4.gif?size=4096"
+);
+       
+})
+
+
+
 
 client.login(process.env.TOKEN);
